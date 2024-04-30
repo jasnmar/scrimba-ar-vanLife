ffc650b1fcf6087b685659ac404ad0e1da23f676
jasnmar <jasnmar@gmail.com>
2024-04-30 09:38:49 -0400

    Some page reorg

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Nav from "./Nav";
 
 /**
  * Challenge:
  * Bootstrap the VanLife project by creating the first 2 routes:
  * Home and About.
- * 
+ *
  * Also include the navbar that can link between the two routes.
  * For now, you'll either need to copy/paste the navbar code
  * to both Home and About pages, or you'll need to find a place
  * to put it where it can be shared between the two pages.
  * (Don't overthink this part - just do whatever is easiest for
  * you because we'll learn a better approach very soon)
- * 
+ *
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <BrowserRouter>
+      <header>
+        <Nav />
+      </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
