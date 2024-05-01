5a812470fd0fde96595c60ba815e2b26cf47c92d
jasnmar <jasnmar@gmail.com>
2024-05-01 15:23:59 -0400

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <>
+      <footer>&#169; 2022 #VANLIFE</footer>
+    </>
+  )
+}
+
+export default Footer
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import "./Layout.css"
 import { Outlet } from "react-router-dom"
 import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
 
 function Layout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
