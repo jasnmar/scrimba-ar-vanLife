b166375f7e25601931c5934770a919c4ed2f6a3c
jasnmar <jasnmar@gmail.com>
2024-05-29 14:38:21 -0400

    Remove Logout

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Vans from "./pages/Van/Vans/Vans";
-import vansLoader from "./pages/Van/Vans/Vans-loader";
+import vansLoader from "./pages/Van/Vans/VansLoader";
 import Layout from "./components/Layout/Layout";
 import Error from "./components/Error/Error";
 import VanDetail from "./pages/Van/VanDetail/VanDetail";
@@ -17,16 +17,16 @@ import Reviews from "./pages/Host/Reviews/Reviews";
 import Income from "./pages/Host/Income/Income";
 import HostLayout from "./components/HostLayout/HostLayout";
 import HostVans from "./pages/Host/Vans/HostVans";
-import HostVansLoader from "./pages/Host/Vans/HostVans-loader";
+import HostVansLoader from "./pages/Host/Vans/HostVansLoader";
 import HostVanDetails from "./pages/Host/Vans/HostVanDetails/HostVanDetails";
 import HostVanLayout from "./components/HostVanLayout/HostVanLayout";
-import HostVanLayoutLoader from "./components/HostVanLayout/HostVanLayout-loader";
+import HostVanLayoutLoader from "./components/HostVanLayout/HostVanLayoutLoader";
 import HostVansPricing from "./pages/Host/Vans/HostVansPricing/HostVansPricing";
 import HostVansPhotos from "./pages/Host/Vans/HostVansPhotos/HostVansPhotos";
 import NotFound from "./pages/NotFound/NotFound";
 import Login from "./pages/Login/Login";
 import LoginAction from "./pages/Login/LoginAction";
-import vanDetailLoader from "./pages/Van/VanDetail/VanDetail-loader";
+import vanDetailLoader from "./pages/Van/VanDetail/VanDetailLoader";
 import requireAuth from "./utils/utils";
 
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -15,7 +15,7 @@ function Nav() {
           <NavLink className={({isActive})=> isActive ? "activeLink nav--item" : "nav--item"} to="about">About</NavLink>
           <NavLink className={({isActive})=> isActive ? "activeLink nav--item" : "nav--item"} to="vans">Vans</NavLink>
           <Link to="login" className="login-link"> <img src={avatar} className="login-icon" /></Link>
-          <button onClick={logOut}>X</button>
+          {/* <button onClick={logOut}>X</button> */}
         </div>
       </nav>
     </>
